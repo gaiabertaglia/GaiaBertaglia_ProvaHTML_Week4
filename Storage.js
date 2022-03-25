14d0016e1e1c2d408a3987f4f05d3eb316812cae
gaiabertaglia <gaia.bertaglia@gmail.com>
2022-03-25 15:11:34 +0100

    Versione finale

---
+++ Storage.js
@@ -0,0 +1,23 @@
+﻿function getStorage() {    
+    
+    let storageName = "localStorage";
+    return window[storageName];
+}
+
+function subscribe() {
+    let storage = getStorage();
+    let key = document.getElementById("storageKey").value;
+    let value = document.getElementById("storageValue").value;
+    storage.setItem(key, value);
+}
+
+function unsubscribe() {
+    let storage = getStorage();
+    let key = document.getElementById("storageKey").value;
+    storage.removeItem(key);
+    document.getElementById("result").innerHTML = "Eliminato item con chiave " + key;
+}
+
+function welcomeMessage() {    
+    alert("Benvenuto!");
+}
